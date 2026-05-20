5410cbb7c8be3113a481c4939a97778892ecddbd
perdanamain <firman.fp123@gmail.com>
2026-05-20 10:37:38 +0700

    fix: vercel static function

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,19 @@ process.loadEnvFile?.();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware parsing form body HTML
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-console.log('path views: ', path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/", route);
 
-app.listen(PORT, () => {
-  console.log(`Server  running on: http://localhost:${PORT}`);
-});
+// Only start the server if not running in Vercel environment
+if (process.env.VERCEL !== "1") {
+  app.listen(PORT, () => {
+    console.log(`Server running on: http://localhost:${PORT}`);
+  });
+}
+
+export default app;
